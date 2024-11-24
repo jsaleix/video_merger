e20651bccb141755dcb13afb8b7bfec81521c173
julian-saleix <julian.saleix@gmail.com>
2024-11-24 19:40:39 +0100

    feat: it works!

--- code/main.go
+++ code/main.go
@@ -6,15 +6,24 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"video_merger/transition"
 
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func main() {
+	merge()
+	fmt.Println("Press 'Enter' to close")
+	var input string
+	fmt.Scanln(&input)
+
+}
+
+func merge() {
 	VALID_VIDEO_FORMATS := []string{".mkv", ".mp4", ".mov"}
 
 	directory := "./"
-	outputPath := filepath.Join(directory, "result.mp4")
+	outputPath := filepath.Join(directory, "result.mkv")
 	videoTextFilePath := filepath.Join(directory, "videos.txt")
 
 	// Listing files
@@ -34,8 +43,8 @@ func main() {
 		videoWithPath := filepath.Join(directory, file.Name())
 		fmt.Println(videoWithPath)
 		videos = append(videos, videoWithPath)
-
 	}
+
 	if len(videos) == 0 {
 		log.Fatalln("No videos found")
 	}
@@ -45,26 +54,29 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	transitionImagePath, transitionVideoPath := transition.CreateTransitionVideo(directory)
+
 	for _, video := range videos {
 		file.WriteString(fmt.Sprintf("file '%s'\n", video))
+		file.WriteString(fmt.Sprintf("file '%s'\n", transitionVideoPath))
 	}
 
-	err = ffmpeg.Input("videos.txt", ffmpeg.KwArgs{"f": "concat", "safe": 0}).Output(outputPath, ffmpeg.KwArgs{"c": "copy"}).OverWriteOutput().Run()
+	fmt.Println("Merging videos...")
+
+	videoInputOpt := ffmpeg.KwArgs{"f": "concat", "safe": 0}
+	// videoOutputOpt := ffmpeg.KwArgs{"c": "copy"}
+	videoOutputOpt := ffmpeg.KwArgs{"preset": "fast", "c:v": "libx264", "c:a": "aac", "crf": 24, "pix_fmt": "yuv420p", "movflags": "faststart"}
+	// videoOutputOpt := ffmpeg.KwArgs{"preset": "fast", "c": "copy", "pix_fmt": "yuv420p"}
+	err = ffmpeg.Input("videos.txt", videoInputOpt).Output(outputPath, videoOutputOpt).OverWriteOutput().Run()
 
 	if err != nil {
 		log.Fatalln(err)
 	} else {
 		fmt.Printf("Video available at: %s\n", outputPath)
 	}
+
 	file.Close()
-
-	err = os.Remove(videoTextFilePath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println("Press 'Enter' to close")
-	var input string
-	fmt.Scanln(&input)
-
+	os.Remove(videoTextFilePath)
+	os.Remove(transitionImagePath)
+	os.Remove(transitionVideoPath)
 }
---
+++ code/transition/transition.go
@@ -0,0 +1,58 @@
+package transition
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"os"
+	"path/filepath"
+	"time"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+)
+
+func CreateTransitionVideo(directory string) (imgPath, videoPath string) {
+	transitionImagePath := filepath.Join(directory, "transition.jpg")
+	transitionVideoPath := filepath.Join(directory, "transition.mkv")
+
+	createTransitionImage(transitionImagePath)
+
+	fmt.Println(transitionVideoPath)
+	inputOpts := ffmpeg.KwArgs{"f": "image2", "loop": 1, "framerate": 1, "t": 2}
+	// inputOpts := ffmpeg.KwArgs{"loop": 1}
+	outputOpts := ffmpeg.KwArgs{"c:v": "libx264", "c:a": "aac", "crf": 18, "pix_fmt": "yuv420p", "movflags": "faststart"}
+	// outputOpts := ffmpeg.KwArgs{"c": "libx264"}
+	ffmpeg.Input(transitionImagePath, inputOpts).Output(transitionVideoPath, outputOpts).OverWriteOutput().Run()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Transition video created")
+	return transitionImagePath, transitionVideoPath
+}
+
+func createTransitionImage(path string) {
+	img := generateImage()
+	saveImage(img, path)
+}
+
+func saveImage(img *image.RGBA, path string) {
+	out, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	err = jpeg.Encode(out, img, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	out.Close()
+}
+
+func generateImage() *image.RGBA {
+	rect := image.Rect(0, 0, 1920, 1080)
+	img := image.NewRGBA(rect)
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 1}}, image.Point{}, draw.Src)
+
+	return img
+}
